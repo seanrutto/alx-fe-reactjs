c6bf86839cb71c890a786c3daa5bf1d4f95d6d2b
Seanrutto <travisean.ts@gmail.com>
2024-08-07 12:03:22 +0300

    spaces removed

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
       <Header />
       <MainContent />
-      <UserProfile name = "Alice" age = "25" bio = "Loves hiking and photography" />
+      <UserProfile name= "Alice" age= "25" bio= "Loves hiking and photography" />
       <Footer />
     </>
   )
