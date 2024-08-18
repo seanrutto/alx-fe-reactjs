95fb2539d25b798cdfe26e82bf8ce0a3983def16
Sean Rutto <travisean.ts@gmail.com>
2024-08-18 17:57:41 +0300

    count

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name= "Alice" age= "25" bio= "Loves hiking and photography" />
+      <Counter />
       <Footer />
     </>
   )
