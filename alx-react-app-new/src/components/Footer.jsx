f8b5021e76b3df506be341345b2bb41f43e430d5
Sean Rutto <travisean.ts@gmail.com>
2024-08-18 17:13:38 +0300

    new file

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer style = {{padding: '5px', margin: '5px'}}>
+            <p style = {{textAlign: 'center', color: 'red'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return(
+        <main style={{ border: '1px solid black', borderRadius: '10px', padding: '20px', margin: '10px' }}>
+            <p style={{ fontWeight: 'italic', color: 'yellow', textAlign: 'center' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent
