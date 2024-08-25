d09ca5a7186dea1943ba79d6561dae5d198e71e2
Sean Rutto <travisean.ts@gmail.com>
2024-08-25 21:35:54 +0300

    done

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,7 +3,17 @@ import  { create }  from 'zustand'
 const useRecipeStore = create((set) => ({
     recipes: [],
     addRecipe: (newRecipe) => set((state) => ({recipes: [...state.recipes, newRecipe]})),
-    setRecipes: (recipes) => set({recipes})
+    updateRecipe: (id, updatedRecipe) => 
+        set((state) => ({
+            recipes: state.recipes.map((recipe) =>
+                recipe.id === id ? {...recipe,...updatedRecipe} : recipe 
+            ),
+        })),
+
+    deleteRecipe: (id) => 
+        set((state) =+ ({
+            recipes: state.recipes.map(((recipe) => recipe.id !== id)),
+        })),
 }))
 
 export default useRecipeStore;
