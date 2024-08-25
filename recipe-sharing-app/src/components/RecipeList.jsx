aff1b0c7df8e74bc1bc844f111961f18c204e7b8
Sean Rutto <travisean.ts@gmail.com>
2024-08-25 13:27:44 +0300

    updated recipe store

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import { useRecipeStore } from '../stores/useRecipeStore'
+import { useRecipeStore } from '../stores/recipeStore'
 
 const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes)
