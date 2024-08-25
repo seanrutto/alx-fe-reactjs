eb3c09a3329385ed8d0bfcf416c915b87283338c
Sean Rutto <travisean.ts@gmail.com>
2024-08-25 13:20:34 +0300

    fixed

--- recipe-sharing-app/src/stores/useRecipeStore.js
+++ recipe-sharing-app/src/stores/useRecipeStore.js
@@ -1,8 +1,8 @@
-import { create } from 'zustand'
+import  { create }  from 'zustand'
 
-const useRecipeStore = create(set => ({
-    recipes: []
-    addRecipe: (newRecipe) => set((state) => ({recipes: [...state.recipes, newRecipe]}))
+const useRecipeStore = create((set) => ({
+    recipes: [],
+    addRecipe: (newRecipe) => set((state) => ({recipes: [...state.recipes, newRecipe]})),
     setRecipes: (recipes) => set({recipes})
 }))
 
