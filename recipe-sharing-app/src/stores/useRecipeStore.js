f24f68b57667a13aaaaaaa4cbb02e364e761a0a8
Sean Rutto <travisean.ts@gmail.com>
2024-08-25 04:13:20 +0300

    yes no maybe

--- recipe-sharing-app/src/stores/useRecipeStore.js
+++ recipe-sharing-app/src/stores/useRecipeStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 
 const useRecipeStore = create(set => ({
     recipes: []
-    addRecipe: (newRecipe) => set(state => ({recipes: [...state.recipes, newRecipe]}))
+    addRecipe: (newRecipe) => set((state) => ({recipes: [...state.recipes, newRecipe]}))
     setRecipes: (recipes) => set({recipes})
 }))
 
