eb55f6d55b784a900406f8b8dc7205b2cb9956a7
Sean Rutto <travisean.ts@gmail.com>
2024-09-10 17:37:02 +0300

    Delete tailwind-react-intergration directory

--- tailwind-react-intergration/README.md
+++
@@ -1,4 +0,0 @@
-# React + Vite
-I am rewriting the Read me
-This is a tailwind test project
-Renamed the readme
--- tailwind-react-intergration/postcss.config.js
+++
@@ -1,6 +0,0 @@
-// postcss.config.js
-export const plugins = {
-  'postcss-import': {},
-  tailwindcss: {},
-  autoprefixer: {},
-};
