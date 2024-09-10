48d403ba9211ed615ef0f0131421a3602dd76cbb
Seanrutto <travisean.ts@gmail.com>
2024-09-10 17:15:05 +0300

    tailwind upload

---
+++ tailwind-react-intergration/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
