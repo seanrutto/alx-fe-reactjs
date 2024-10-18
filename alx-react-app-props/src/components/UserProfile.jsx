c0b9043d901d1c03aa107205f44d6eed237602e8
Seanrutto <travisean.ts@gmail.com>
2024-10-18 15:46:53 +0300

    Trres

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,14 @@
-function UserProfile(props){
-    return(
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
+
+function UserProfile() {
+    const userData = useContext(UserContext); // Use context to access user data
+
+    return (
         <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
+            <h2>{userData.name}</h2> 
+            <p>Age: {userData.age}</p> 
+            <p>Bio: {userData.bio}</p> 
         </div>
     );
 }
