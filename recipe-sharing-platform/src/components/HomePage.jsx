e2821ff6e35063b2eadc667dba703a9542ece5ae
Seanrutto <travisean.ts@gmail.com>
2024-10-18 22:22:25 +0300

    maybe

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,3 +1,10 @@
-import React from "react";
+
 import { useState, useEffect } from "react";
 
+const DataContainer = () => {
+    const [ data, useData ] = useState([])
+
+    useEffect(() => {
+        
+    },[])
+}
