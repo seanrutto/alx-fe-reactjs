885c04073bddb1321ca4347b1cd6c30e5d245ab1
Seanrutto <travisean.ts@gmail.com>
2024-10-18 15:57:15 +0300

    little bit of styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#282c34', color: 'white' }}>
+    <nav style={{ display: 'flex', justifyContent:'space-between', padding: '10px', backgroundColor: '#282c34', color: 'white' }}>
       <Link to="/" style={{ margin: '0 15px', color: 'white', textDecoration: 'none' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 15px', color: 'white', textDecoration: 'none' }}>About</Link>
       <Link to="/services" style={{ margin: '0 15px', color: 'white', textDecoration: 'none' }}>Services</Link>
