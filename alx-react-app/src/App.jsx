5b646f6103604e2e5e22087819f61554d549db34
Seanrutto <travisean.ts@gmail.com>
2024-10-18 21:45:00 +0300

    app correction

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+
+function App() {
+  
+
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  )
+}
+
+export default App
